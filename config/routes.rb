5171fcded9bdeae279cba0acadbf98c6e02fb955
yuya0125 <62539180+yuya0125@users.noreply.github.com>
2020-04-23 17:33:19 +0900

    edit chatspace

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  # get 'messages/index'
+  devise_for :users
+  root 'groups#index'
   root "messages#index"
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create]
+  root "messages#index"
+  resources :groups, only: [:index, :new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
