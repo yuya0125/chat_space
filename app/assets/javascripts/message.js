644bf33b411c251cad05a8b99a26b6a6b430246c
yuya0125 <62539180+yuya0125@users.noreply.github.com>
2020-05-14 19:07:47 +0900

    ajax0-fix

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -52,13 +52,9 @@ $('#new_message').on('submit', function(e){
    contentType: false
  })
   .done(function(data){
-    
     var html = buildHTML(data);
     $('.messages').append(html);
     $('form')[0].reset();
-    $('.box').animate({'height' : '200px'});
-    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-  }
-  .fail(function() {
-      alert("メッセージ送信に失敗しました");
-  });
+  })
+})
+});
