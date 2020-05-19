4c103509c057490867ca6c75094f924e8308fbb5
yuya0125 <62539180+yuya0125@users.noreply.github.com>
2020-05-19 18:22:42 +0900

    UPDATEーFIX

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
